834d6b5087613af4cb5e7ce318741cb9ed1d8e9e
Dale <rd01748@georgiasouthern.edu>
2019-06-05 15:27:42 -0400

    Add server.js file which connects app to DB.

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const connectToDB = require('./utils/connectToDB');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+connectToDB();
+
+app.use('/api/hitters', require('./routes/hitters'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Server started on: ${port}`);
+});
