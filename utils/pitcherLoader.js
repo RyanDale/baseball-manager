fecbf33a46e59da076cc8875684ab73c57ce8a8f
Dale <rd01748@georgiasouthern.edu>
2019-06-06 10:00:51 -0400

    Create pitcherLoader script to import pitchers from csv.

---
+++ utils/pitcherLoader.js
@@ -0,0 +1,55 @@
+const connectToDB = require('./connectToDB');
+const { addCards, transformPlayer } = require('./importHelpers');
+
+const csv = require('csv-parser');
+const fs = require('fs');
+const _ = require('lodash');
+
+const Pitcher = require('../models/Pitcher');
+
+
+function setCMD(pitcher) {
+    if (parseInt(pitcher.cmdPlus) === 0) {
+        pitcher.cmdPlus = {};
+        return pitcher;
+    }
+    const cmd = pitcher.cmdPlus.split('+');
+    const boost = cmd[1];
+    pitcher.cmdPlus = {};
+    pitcher.cmdPlus[cmd[0] === 'L' ? 'left' : 'right'] = boost;
+    return pitcher;
+}
+
+connectToDB().then(() => {
+    const readStream = fs.createReadStream('./assets/pitchers2019.csv');
+
+    const alterKeyNames = [
+        ['hnd', 'hand'],
+        ['sal', 'salary'],
+        ['clt', 'clutch'],
+        ['x-r', 'xRange'],
+        ['fld', 'fielding'],
+    ];
+    const resultKeys = [
+        'k', 'gb', 'fb', 'bb', '_1B', '_2B', 'hr'
+    ];
+    const intKeys = ['salary', 'clutch', 'fielding', 'cmd', 'x-r'];
+
+    let records = [];
+    readStream.pipe(csv())
+        .on('data', record => records.push(record))
+        .on('end', () => {
+            readStream.destroy();
+            records = records.map(player => transformPlayer(player, alterKeyNames, resultKeys, intKeys));
+            addCards(records);
+            Pitcher.deleteMany({}, () => {
+                const pitchers = records.map(record => new Pitcher(record)).map(setCMD);
+                Pitcher.insertMany(pitchers).then(docs => {
+                    console.log('Success', docs.length);
+                }).catch(err => {
+                    console.log('Error', err);
+                    process.exit();
+                });
+            });
+        });
+});
