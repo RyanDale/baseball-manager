3c59b0dea268afdd3ef9e30f9e449aa3643a7f1c
Dale <rd01748@georgiasouthern.edu>
2019-06-16 21:46:35 -0400

    Add thumbnails for pitchers in the team details view.

--- client/src/components/TeamDetail.js
+++ client/src/components/TeamDetail.js
@@ -6,6 +6,7 @@ import _ from 'lodash';
 
 import { getTeam } from '../actions/teamActions';
 import PlayerRemove from './PlayerRemove';
+import PlayerDetailModal from './PlayerDetailModal';
 
 
 class TeamDetail extends Component {
@@ -14,12 +15,38 @@ class TeamDetail extends Component {
         team: PropTypes.object.isRequired
     };
 
+    constructor(props, context) {
+        super(props, context);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
+        this.state = {
+            activePlayer: {},
+            show: false
+        };
+    }
+
+    handleClose() {
+        this.setState({
+            activePlayer: {},
+            show: false
+        });
+    }
+
+    handleShow(player) {
+        this.setState({
+            activePlayer: player,
+            show: true
+        });
+    }
+
     componentDidMount() {
         this.props.getTeam(this.props.match.params.id);
     }
 
     render() {
         const { team, loading } = this.props.team;
+        const { show, activePlayer } = this.state;
         const hitters = _.get(team, 'hitters', []);
         const pitchers = _.get(team, 'pitchers', []);
         const [startingPitchers, reliefPitchers] = _.partition(pitchers, pitcher => pitcher.primaryPosition === 'SP');
@@ -30,103 +57,116 @@ class TeamDetail extends Component {
                 <Card body>
                     <Card.Title>Hitters ({hitters.length} of 12+) {hitters.reduce((value, p) => p.salary + value, 0)} points</Card.Title>
                     {
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Position</th>
-                                <th>Salary</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                hitters.map(player => (
-                                    <tr key={player._id}>
-                                        <td>
-                                            <PlayerRemove
-                                                playerType="hitters"
-                                                player={player}>
-                                            </PlayerRemove>
-                                        </td>
-                                        <td>{ player.firstName }</td>
-                                        <td>{ player.lastName }</td>
-                                        <td>{ player.positionList.join(', ') }</td>
-                                        <td>{ player.salary }</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </Table>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Position</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    hitters.map(player => (
+                                        <tr key={player._id}>
+                                            <td>
+                                                <PlayerRemove
+                                                    playerType="hitters"
+                                                    player={player}>
+                                                </PlayerRemove>
+                                            </td>
+                                            <td>{player.firstName}</td>
+                                            <td>{player.lastName}</td>
+                                            <td>{player.positionList.join(', ')}</td>
+                                            <td>{player.salary}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
                     }
                 </Card>
                 <br />
                 <Card body>
                     <Card.Title>Starting Pitchers ({startingPitchers.length} of 5), {startingPitchers.reduce((value, p) => p.salary + value, 0)} points</Card.Title>
                     {
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Salary</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                startingPitchers.map(player => (
-                                    <tr key={player._id}>
-                                        <td>
-                                            <PlayerRemove
-                                                playerType="pitchers"
-                                                player={player}>
-                                            </PlayerRemove>
-                                        </td>
-                                        <td>{ player.firstName }</td>
-                                        <td>{ player.lastName }</td>
-                                        <td>{ player.salary }</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </Table>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Card</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    startingPitchers.map(player => (
+                                        <tr key={player._id}>
+                                            <td>
+                                                <PlayerRemove
+                                                    playerType="pitchers"
+                                                    player={player}>
+                                                </PlayerRemove>
+                                            </td>
+                                            <td>
+                                                <img onClick={() => this.handleShow(player)} height="100" src={`/api/pitchers/${player._id}/card`} />
+                                            </td>
+                                            <td>{player.firstName}</td>
+                                            <td>{player.lastName}</td>
+                                            <td>{player.salary}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
                     }
                 </Card>
                 <br />
                 <Card body>
                     <Card.Title>Relief Pitchers ({reliefPitchers.length} of 7+), {reliefPitchers.reduce((value, p) => p.salary + value, 0)} points</Card.Title>
                     {
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Salary</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                reliefPitchers.map(player => (
-                                    <tr key={player._id}>
-                                        <td>
-                                            <PlayerRemove
-                                                playerType="pitchers"
-                                                player={player}>
-                                            </PlayerRemove>
-                                        </td>
-                                        <td>{ player.firstName }</td>
-                                        <td>{ player.lastName }</td>
-                                        <td>{ player.salary }</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </Table>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Card</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    reliefPitchers.map(player => (
+                                        <tr key={player._id}>
+                                            <td>
+                                                <PlayerRemove
+                                                    playerType="pitchers"
+                                                    player={player}>
+                                                </PlayerRemove>
+                                            </td>
+                                            <td>
+                                                <img onClick={() => this.handleShow(player)} height="100" src={`/api/pitchers/${player._id}/card`} />
+                                            </td>
+                                            <td>{player.firstName}</td>
+                                            <td>{player.lastName}</td>
+                                            <td>{player.salary}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
                     }
                 </Card>
+                <PlayerDetailModal player={activePlayer}
+                    playerType='pitcher'
+                    show={show}
+                    closeModal={this.handleClose}>
+                </PlayerDetailModal>
             </Container>
         );
     }
